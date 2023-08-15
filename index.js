fddf12a9c43670cc45e84165381bc76534bca6ab
Brandi Burnell <brandiburnell6@gmail.com>
2023-08-14 19:03:54 -0600

    passed tests and completed lab

---
+++ index.js
@@ -0,0 +1,36 @@
+// adds two numbers and returns the result
+function add(a, b) {
+    return a + b;
+}
+
+// subtracts
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiplies
+function multiply(a, b) {
+    return a * b;
+}
+
+// divides
+function divide(a, b) {
+    return a / b;
+}
+
+// increments n and returns the result
+function increment(n) {
+    return n+= 1;
+}
+
+function decrement(n) {
+    return n -= 1;                                                                                                                        
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
